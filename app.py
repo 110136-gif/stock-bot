a750c161a936f0974bb60f7bbd940ba68a353740
110136-gif <110136@mjcskg.tp.edu.tw>
2026-07-18 12:07:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 # 請務必填入你的真實 Token (這兩行不要刪掉，只改引號內的文字)
 line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
+line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
-@app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
